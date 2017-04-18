c51c4494fd06e2b0956e711a2ac62d24089bd31a
RevenueWire <swang@revenuewire.com>
2017-04-18 17:32:02 +0000

    proper response

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Adding production setting, ready for production test"
+    release_note="proper response"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
