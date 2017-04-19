ad7fb6bdf0d60f1adc44a3cfb20fa5319bebe8c0
RevenueWire <swang@revenuewire.com>
2017-04-19 19:17:33 +0000

    rebuild with mbstring and cache directory for mustache

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="proper response"
+    release_note="rebuild with mbstring and cache directory for mustache"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
