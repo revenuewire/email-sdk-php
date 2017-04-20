5a12b91ded3cd4ac4a16622d19d36df76d054677
RevenueWire <swang@revenuewire.com>
2017-04-20 00:11:54 +0000

    upgrade jwt and services

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="give another try"
+    release_note="upgrade jwt and services"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
